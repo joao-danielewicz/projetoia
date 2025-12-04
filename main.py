82c02bedc67135031196a529d1af24d96f22a278
João Danielewicz <joao.wicz@gmail.com>
2025-12-03 23:51:45 -0300

    final

--- main.py
+++ main.py
@@ -3,20 +3,21 @@ import numpy as np
 from objects.Medicamento import Medicamento
 from objects.ConsumoMensal import ConsumoMensal
 from objects.Database import Database
-from objects.MotorInferencia import MotorInferencia as Motor
+from objects.MotorInferencia import MotorInferenciaAuto as MotorAuto
+from objects.MotorInferencia import MotorInferenciaQuest as MotorQ
 from objects.MotorInferencia import *
 from datetime import datetime
 db = Database()
 params = db.SelectParams()
 
-
+# CÁLCULO DE MÉDIA DE CONSUMO
 def CalculoMedia(consumos):
     valoresConsumo = []
     for consumo in consumos:
         valoresConsumo.append(consumo[1])
     return (sum(valoresConsumo) / len(valoresConsumo))
 
-
+# LISTAGEM DE TODOS OS MEDICAMENTOS
 def ListarMedicamentos():
     medicamentos = db.SelectMedicamentos()
     print("=========== LISTA DE MEDICAMENTOS ===========")
@@ -24,6 +25,8 @@ def ListarMedicamentos():
         medicamento = Medicamento(registro[1], registro[2], registro[3], registro[0])
         medicamento.print()
 
+
+# LISTAGEM DE TODOS OS CONSUMOS DE UM DETERMINADO MEDICAMENTO
 def ListarConsumos(idMedicamento):
     consumos = db.SelectConsumos(idMedicamento)
     print("=========== LISTA DE CONSUMOS ===========")
@@ -35,7 +38,7 @@ def ListarConsumos(idMedicamento):
 while True:
     # ================= MENU GERAL =================
     print("=========== OPÇÕES ===========")
-    opcao = int(input("1 - Medicamentos \n 2 - Consumo \n 3 - Editar parâmetros \n 4 - Executar conferências \n 0 - Sair"))
+    opcao = int(input("1 - Medicamentos \n 2 - Consumo \n 3 - Editar parâmetros \n 4 - Executar conferências automáticas\n5 - Executar conferências com questionário \n 0 - Sair"))
     match opcao:
         case 0:
             break
@@ -46,8 +49,9 @@ while True:
             opcao = int(input("1 - Cadastro \n 2 - Visualizar \n 3 - Ver média de consumo mensal \n4 - Atualizar estoque\n 0 - Voltar"))
             match opcao:
                 case 0:
-                    break
+                    pass
                 case 1:
+                    # CADASTRO DE MEDICAMENTO. USO DE DATA
                     os.system('cls')
                     p1 = Medicamento(input("Nome: "),
                       (
@@ -75,7 +79,7 @@ while True:
             opcao = int(input("1 - Cadastro \n 2 - Visualizar \n 0 - Voltar"))
             match opcao:
                 case 0:
-                    break;
+                    pass
                 case 1:
                     ListarMedicamentos()
                     idMedicamento = int(input("Para qual medicamento deseja cadastrar consumo?"))
@@ -88,28 +92,53 @@ while True:
                     ListarConsumos(idMedicamento)
 
         case 3: 
+            # CONSULTA E ATUALIZAÇÃO DOS PARÂMETROS DE SEGURANÇA. POR PADRÃO, A MARGEM DE SEGURANÇA É 20%
+            # E O ALERTA DE VENCIMENTO É DE 90 DIAS
             print("Margem de segurança: ", (params[0]*100),"%\nIntervalo de alerta de vencimento próximo: ", params[1], " dias.")
             print("=========== EDIÇÃO ===========")
             db.UpdateParams([(float(input("Porcentagem da margem de segurança: ")))/100, int(input("\nAlerta de vencimento próximo: "))])
             params = db.SelectParams()
 
         case 4:
-            motor = Motor()
+            # CRIAÇÃO DO MOTOR AUTOMÁTICO, SEM PERGUNTAS AO USUÁRIO
+            motor = MotorAuto()
+            motor.reset()
+            # LOOP PARA VERIFICAR ALERTAS PARA TODOS OS MEDICAMENTOS
+            for medicamento in db.SelectMedicamentos():
+                try:
+                    consumos = db.SelectConsumos(medicamento[0])
+                    
+                    mediaConsumo = CalculoMedia(consumos)
+                    desvioPadrao = np.std(consumos)
+
+                    fator_margem = 1 + (params[0] / 100)
+
+                    # PREVISÃO DE CONSUMO. SOMA A MÉDIA COM O DESVIO PADRÃO E AUMENTA O VALOR COM BASE NA MARGEM DE SEGURANÇA
+                    previsao = np.floor((mediaConsumo + desvioPadrao) * fator_margem)
+
+                    motor.declare(Estoque(quantidade = medicamento[3], nome = medicamento[1]))
+                    motor.declare(Previsao(estoque = medicamento[3], nome = medicamento[1], previsao = previsao))
+                    motor.declare(Validade(data = medicamento[2], nome = medicamento[1], diasAlerta = params[1]))
+                    motor.declare(MediaConsumo(media = mediaConsumo, nome = medicamento[1], estoque = medicamento[3], seguranca = params[0]))
+                except:
+                    pass
+
+            motor.run()
+
+        case 5:
+            motor = MotorQ()
             motor.reset()
             for medicamento in db.SelectMedicamentos():
-                consumos = db.SelectConsumos(medicamento[0])
-                
-                mediaConsumo = CalculoMedia(consumos)
-                desvioPadrao = np.std(consumos)
+                nome = medicamento[1]
+                print("=============== ", nome, " ===============")
+                essencial = input("É essencial? (s/n): ").lower() == "s"
+                fornecedor = input("Possui fornecedor alternativo? (s/n): ").lower() == "s"
+                reposicao = int(input("Tempo de reposição (dias): "))
+                critico = input("Uso crítico/hospitalar? (s/n): ").lower() == "s"
+                print("===================================")
 
-                fator_margem = 1 + (params[0] / 100)
-
-                # Previsão de consumo
-                previsao = np.floor((mediaConsumo + desvioPadrao) * fator_margem)
-
-                motor.declare(Estoque(quantidade = medicamento[3], nome = medicamento[1]))
-                motor.declare(Previsao(estoque = medicamento[3], nome = medicamento[1], previsao = previsao))
-                motor.declare(Validade(data = medicamento[2], nome = medicamento[1]))
-                motor.declare(MediaConsumo(media = mediaConsumo, nome = medicamento[1], estoque = medicamento[3], seguranca = params[0]))
-            
+                motor.declare(Essencial(nome=nome, valor=essencial))
+                motor.declare(FornecedorAlternativo(nome=nome, existe=fornecedor))
+                motor.declare(TempoReposicao(nome=nome, dias=reposicao))
+                motor.declare(MedicamentoCritico(nome=nome, valor=critico))
             motor.run()
--- objects/MotorInferencia.py
+++ objects/MotorInferencia.py
@@ -14,13 +14,14 @@ class Validade(Fact):
 class Previsao(Fact):
     pass
 
-class MotorInferencia(KnowledgeEngine):
+class MotorInferenciaAuto(KnowledgeEngine):
+
     @Rule(Estoque(quantidade=0, nome = MATCH.nome))
     def estoque_zerado(self, nome):
         print("O estoque de ", nome, "está zerado.")
 
 
-
+    # VERIFICA SE O ESTOQUE SERÁ SUFICIENTE PARA SUPRIR A POSSÍVEL DEMANDA ESTIMADA
     @Rule(Previsao(estoque=MATCH.estoque, nome=MATCH.nome, previsao = MATCH.previsao), 
           TEST(lambda estoque, previsao: estoque < previsao))              
     def estoque_insatisfatorio(self, nome):
@@ -32,14 +33,16 @@ class MotorInferencia(KnowledgeEngine):
         print("O estoque de ", nome, " é satisfatório.")
     
 
-    @Rule(Validade(data = MATCH.data, nome = MATCH.nome),
-           TEST(lambda data: ((data - datetime.now()).days) > 90))
+
+    # VERIFICAÇÃO DA DATA DE VALIDADE COM BASE NO PARÂMETRO DEFINIDO.
+    @Rule(Validade(data = MATCH.data, nome = MATCH.nome, diasAlerta = MATCH.diasAlerta),
+           TEST(lambda data, diasAlerta: ((data - datetime.now()).days) > diasAlerta))
     def risco_vencimento(self, nome, data):
         dias = (data - datetime.now()).days
         print("O medicamento ", nome, " não representa risco de vencimento. ", data)
     
-    @Rule(Validade(data = MATCH.data, nome = MATCH.nome),
-           TEST(lambda data: 1 < ((data - datetime.now()).days) < 90))
+    @Rule(Validade(data = MATCH.data, nome = MATCH.nome, diasAlerta = MATCH.diasAlerta),
+           TEST(lambda data, diasAlerta: 1 < ((data - datetime.now()).days) < diasAlerta))
     def risco_vencimento(self, nome, data):
         dias = (data - datetime.now()).days
         print("O medicamento ", nome, " vencerá em ", dias, "dias, na data ", data)
@@ -50,6 +53,9 @@ class MotorInferencia(KnowledgeEngine):
         print("O medicamento ", nome, " está vencido! Remova-o do estoque! ")
     
     
+
+    # vERIFICA SE A MÉDIA DE CONSUMO É MENOR OU MAIOR QUE UMA PORCENTAGEM DO ESTOQUE.
+    # COM BASE NISSO, INFORMA SE HÁ UM BAIXO OU ALTO CONSUMO
     @Rule(
         MediaConsumo(nome=MATCH.nome, media=MATCH.media, estoque = MATCH.estoque, seguranca = MATCH.seguranca),
         TEST(lambda media, estoque, seguranca: media > estoque*seguranca)
@@ -63,3 +69,56 @@ class MotorInferencia(KnowledgeEngine):
     )
     def baixo_consumo(self, nome):
         print("Há um consumo baixo de ", nome)
+
+
+class Essencial(Fact):
+    pass
+
+class TempoReposicao(Fact):
+    pass
+
+class FornecedorAlternativo(Fact):
+    pass
+
+class MedicamentoCritico(Fact):
+    pass
+
+class MotorInferenciaQuest(KnowledgeEngine):
+    # VERIFICA SE UM MEDICAMENTO É ESSENCIAL E SE HÁ FORNECEDOR ALTERNATIVO PARA O MESMO
+    @Rule(
+        Essencial(nome=MATCH.nome, valor=True),
+        FornecedorAlternativo(nome=MATCH.nome, existe=False)
+    )
+    def prioridade_maxima(self, nome):
+        print("PRIORIDADE MÁXIMA para o medicamento", nome)
+
+
+    # VERIFICA SE HÁ UMA DEMORA NA REPOSIÇÃO DO MEDICAMENTO
+    @Rule(
+        TempoReposicao(nome=MATCH.nome, dias=MATCH.dias),
+        TEST(lambda dias: dias > 20)
+    )   
+    def reposicao_lenta(self, nome, dias):
+        print(f"Atenção: reposição lenta de {nome} ({dias} dias)")
+
+    @Rule(
+        Essencial(nome=MATCH.nome, valor=False),
+        FornecedorAlternativo(nome=MATCH.nome, existe=True)
+    )
+    def situacao_confortavel(self, nome):
+        print("Medicamento", nome, "em situação confortável")
+
+
+    # ANALISA SE UM MEDICAMENTO TEM CONSUMO ELEVADO E É DE ATENÇÃO CRÍTICCA.
+    @Rule(
+        MediaConsumo(nome=MATCH.nome, media=MATCH.media),
+        MedicamentoCritico(nome=MATCH.nome, valor=True),
+        TEST(lambda media: media > 0)
+    )
+    def risco_operacional(self, nome):
+        print("RISCO OPERACIONAL: medicamento crítico com consumo elevado:", nome)
+
+
+
+
+
